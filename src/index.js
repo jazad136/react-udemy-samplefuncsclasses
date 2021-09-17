1701924c519ab751fd2b7cd1d965a7e46a8c05f9
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-16 21:12:16 -0400

    Style Component Practice - Fixed Using Style Prop

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const App = () => {
         <div>
             <label class="label" for="name">Enter name:</label>
             <input id="name" type="text"/>
-            <button style="background-color: blue; color: white">Submit</button>
+            <button style={{ backgroundColor: 'blue', color: 'white'}}>
+                Submit
+            </button>
         </div>
     );
 }
