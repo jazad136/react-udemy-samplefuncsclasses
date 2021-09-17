574dc1b406bd77aa88db64795aa251461b4ce19d
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-16 21:55:04 -0400

    JavaScript Practice - Add Time call to index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ import ReactDOM from 'react-dom'
 
 // Create a react component
 
+function getTime() { 
+    return (new Date()).toLocaleTimeString()
+}
 const App = () => {
     // const buttonText = 'Click Me!';
     // const buttonText = ['Hi', 'There'];
     // const buttonText = ['10', '20'];
+
     const buttonText = {text: 'Click me now'}
     return (
         <div>
-            <label className="label" for="name">Enter name:</label>
+            <div>Current Time</div>
+            <h3>{getTime()}</h3>
+            <label className="label" htmlFor="name">Enter name:</label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor: 'blue', color: 'white'}}>
                 {buttonText.text}
             </button>
+
+            
         </div>
     );
 }
