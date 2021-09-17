4b6f88e9981aad4c4cafa6c473f9e8bdd85a8f36
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-16 21:34:13 -0400

    JavaScript Practice - Reference a JSON Object Attribute

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Create a react component
-function getButtonText() { 
-    return 'Click on me!';
-}
+
 const App = () => {
-    //const buttonText = 'Click Me!';
+    // const buttonText = 'Click Me!';
+    // const buttonText = ['Hi', 'There'];
+    // const buttonText = ['10', '20'];
+    const buttonText = {text: 'Click me now'}
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text"/>
             <button style={{ backgroundColor: 'blue', color: 'white'}}>
-                {getButtonText()}
+                {buttonText.text}
             </button>
         </div>
     );
