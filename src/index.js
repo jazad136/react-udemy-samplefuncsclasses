cbbe292009025fa389cb146cd73ad5b8513201ba
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-16 21:10:34 -0400

    Style Component Practice - Broken

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import ReactDOM from 'react-dom'
 // Create a react component
 
 const App = () => {
-    return <div>Hi there!</div>
-
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style="background-color: blue; color: white">Submit</button>
+        </div>
+    );
 }
 
 
@@ -16,4 +21,4 @@ const App = () => {
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-)
+)   
