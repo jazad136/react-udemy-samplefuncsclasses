413c21b7d2194cfe9a0c46f553a4b668985924e5
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-21 09:53:07 -0400

    Create components project

---
+++ components/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hi there!</div>
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
