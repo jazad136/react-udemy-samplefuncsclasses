e497cb5722076720fe4b97edc13d41260d30acf8
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-21 09:53:24 -0400

    Removing jsx files from top level

--- sampleBabel/sampleBabelIndex.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-const App = () => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Hi There"), /*#__PURE__*/React.createElement("li", null, "By There"), /*#__PURE__*/React.createElement("li", null, "Gotcha")));
-};
