2c0962189bcdbda25db932e09ea4cbe54702d0ed
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-09-23 20:01:25 -0400

    Make Props from author timeAgo content avatarSrc

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = props => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={Faker.image.avatar()}/>
+                <img alt="avatar" src={props.avatarSrc}/>
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
+                    {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{props.content}</div>
             </div>
         </div>
     );
