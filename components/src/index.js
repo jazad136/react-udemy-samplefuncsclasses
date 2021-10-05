e52c06fb44c5dca25eed0450c10d5c8eeea6c634
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-05 17:15:01 -0400

    Wrap each comment in a SemanticUI Card Element

--- components/src/index.js
+++ components/src/index.js
@@ -2,25 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
-
+import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                content="I do what I can."
-                avatarSrc={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 2:00PM"
-                content="Thank you for the compliment."
-                avatarSrc={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Yesterday at 5:00PM"
-                content="Good job on the blog post."
-                avatarSrc={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="I do what I can."
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00PM"
+                    content="Thank you for the compliment."
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM"
+                    content="Good job on the blog post."
+                    avatarSrc={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
