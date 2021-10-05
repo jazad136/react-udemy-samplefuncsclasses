9bd1e1e30a5f9c63f1e9300b436d6d365ff4dc59
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-05 16:59:50 -0400

    Use conventional faker syntax from tutorial

--- components/src/index.js
+++ components/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker'
 import CommentDetail from './CommentDetail'
-import Faker from 'faker'
+
 const App = () => {
     return (
         <div className="ui container comments">
@@ -9,17 +10,17 @@ const App = () => {
                 author="Sam" 
                 timeAgo="Today at 4:45PM" 
                 content="I do what I can."
-                avatarSrc={Faker.image.avatar()}/>
+                avatarSrc={faker.image.avatar()}/>
             <CommentDetail 
                 author="Alex" 
                 timeAgo="Today at 2:00PM"
                 content="Thank you for the compliment."
-                avatarSrc={Faker.image.avatar()}/>
+                avatarSrc={faker.image.avatar()}/>
             <CommentDetail 
                 author="Jane" 
                 timeAgo="Yesterday at 5:00PM"
                 content="Good job on the blog post."
-                avatarSrc={Faker.image.avatar()}/>
+                avatarSrc={faker.image.avatar()}/>
         </div>
     );
 };
