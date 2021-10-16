08ddf371c28da21faa7fd2eca53d50cb6548f14a
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-10-15 20:02:14 -0400

    Adding Geolocation Permission

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+    );
     return <div>Hi there.</div>
 };
 
-ReactDOM.render(<App />,document.querySelector('#root'))
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
