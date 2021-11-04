35d67502b8f2cdd63c2c1902a3353dd91bcbe0a7
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-04 14:31:42 -0400

    Catch Error message and print

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,19 +7,29 @@ class App extends React.Component {
 
         // here we have to initialize the state
         // using the equal sign. 
-        this.state = {lat: null}
+        this.state = {lat: null, errorMessage: ''}
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setState to update the position
                 // not set the state element. 
-                this.setState({lat : position.coords.latitude})
+                this.setState({lat: position.coords.latitude})
             },
-            err => console.log(err)
+            // err => console.log(err)
+            err => {
+                this.setState({errorMessage: err.message})
+                console.log(err)
+            }
         ); 
     }
 
     render() { 
-        return <div>Latitude: {this.state.lat}</div>;
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br/>
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 };
 
