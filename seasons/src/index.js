ef5356b1b198057dff0127400bc9f1900022f0f6
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-16 11:17:38 -0500

    Moved constructor state init to open space above methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    constructor(props) { 
-        super(props);
 
-        // here we have to initialize the state
-        // using the equal sign. 
-        this.state = {lat: null, errorMessage: ''}
-        
-    }
-
+    state = {lat: null, errorMessage: ''};
+    
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
             err => this.setState({errorMessage: err.message})
         ); 
     }
+    
     render() { 
         if(this.state.errorMessage && !this.state.lat) { 
             return <div>Error: {this.state.errorMessage}</div>
