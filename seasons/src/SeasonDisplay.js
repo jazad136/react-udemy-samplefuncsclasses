b5012ff4834cad4bfefeee22447f944302dda2f5
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-23 10:21:36 -0500

    Use latitude to display season

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const getSeason = (lat, month) => { 
+    if(month > 2 && month < 9) { 
+        return lat > 0 ? 'summer' : 'winter';   
+    } else { 
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
 const SeasonDisplay = props => { 
-    console.log(props.lat)
-    return <div>Season Display</div>;
+    const season = getSeason(props.lat, new Date().getMonth());
+    return (
+        <div>
+            <div>Season Display</div>
+            <div>{season}</div>
+        </div>
+    );
 }
 export default SeasonDisplay;
