f2d919eca409a7ad553e57903057527c6b29896f
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-23 20:49:57 -0500

    Pass getSeason() to a config object, used to create two app variables text and iconName

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import "semantic-ui-css/semantic.min.css";
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it's chilly!",
+        iconName: 'snowflake'
+    }
+}
 const getSeason = (lat, month) => { 
     if(month > 2 && month < 9) { 
         return lat > 0 ? 'summer' : 'winter';   
@@ -11,13 +20,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => { 
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === "winter" ? "Brr, it is chilly" : "Lets hit the beach" 
-    const icon = season === 'winter' ? 'snowflake' : 'sun';
+    // const confRes = seasonConfig[season] 
+    // then can use confRes.iconName to get iconName
+    // instead use destructured object assignment
+    const {text, iconName} = seasonConfig[season]
+    
     return (
       <div>
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
         <h1>{text}</h1> 
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
       </div>
     );
 }
