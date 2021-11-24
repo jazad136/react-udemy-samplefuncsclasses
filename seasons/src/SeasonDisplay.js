f29c54e175e50e6fc91bc80127943717b0e93ada
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2021-11-23 21:23:58 -0500

    Colorful icons and background now display

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,5 @@
+import './SeasonDisplay.css';
 import React from 'react';
-
 const seasonConfig = {
     summer: {
         text: "Let's hit the beach!",
@@ -26,10 +26,10 @@ const SeasonDisplay = props => {
     const {text, iconName} = seasonConfig[season]
     
     return (
-      <div>
-        <i className={`${iconName} icon`} />
+      <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon `} />
         <h1>{text}</h1> 
-        <i className={`${iconName} icon`} />
+        <i className={`icon-right massive ${iconName} icon`} />
       </div>
     );
 }
