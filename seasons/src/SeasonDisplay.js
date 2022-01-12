006ba8db3ac3c7946c95d6b206f3b37c52a92601
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-11 20:20:07 -0500

    Adding timeclock to Seasons Display

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,6 @@
 import './SeasonDisplay.css';
 import React from 'react';
+import Clock from './Clock';
 const seasonConfig = {
     summer: {
         text: "Let's hit the beach!",
@@ -27,9 +28,14 @@ const SeasonDisplay = props => {
     
     return (
       <div className={`season-display ${season}`}>
-        <i className={`icon-left massive ${iconName} icon `} />
+        <div style={{display: 'block', clear: 'both'}}>
+            <i className={`icon-left massive ${iconName} icon `} />
         <h1>{text}</h1> 
         <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+        <div style={{display: 'block', clear: 'both'}}>
+            <Clock />
+        </div>
       </div>
     );
 }
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
-
+// import Clock from './Clock';
 class App extends React.Component {
 
     state = {lat: null, errorMessage: ''};
@@ -19,7 +19,11 @@ class App extends React.Component {
             return <div>Error: {this.state.errorMessage}</div>
         }
         else if(!this.state.errorMessage && this.state.lat) { 
-            return <SeasonDisplay lat={this.state.lat}/>
+            return (
+            <div>
+                <SeasonDisplay lat={this.state.lat} />
+            </div>
+            )
         }
         /*return <div>Loading!</div>*/
         return <Spinner message="Please accept location request" />
