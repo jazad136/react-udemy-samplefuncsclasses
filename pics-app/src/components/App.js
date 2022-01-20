0e5e693312a1cbaf5e8f1afc38b2e79a430ce95a
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-20 10:42:49 -0500

    Add SearchBar to App, a single input form element

--- pics-app/src/components/App.js
+++ pics-app/src/components/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-
+import SearchBar from './SearchBar'
 class App extends Component { 
     render() { 
         return (
-            <div>App</div>
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar />
+            </div>
         );
     }
 }
---
+++ pics-app/src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    render() { 
+        return (
+          <div className="ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
+                    <input type="text"/>
+                </div>
+            </form>
+          </div>
+        );
+    }
+}
+export default SearchBar;
