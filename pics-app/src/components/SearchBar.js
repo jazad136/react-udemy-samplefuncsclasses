9b46346d1d7f7fabf462d6688f08cdaf0a33a156
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-20 15:46:14 -0500

    Add onInputChange callback

--- pics-app/src/components/SearchBar.js
+++ pics-app/src/components/SearchBar.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    onInputChange(event) { 
+        console.log(event.target.value)
+    }
     render() { 
         return (
           <div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text"/>
+                    <input type="text" onChange={this.onInputChange}/>
                 </div>
             </form>
           </div>
