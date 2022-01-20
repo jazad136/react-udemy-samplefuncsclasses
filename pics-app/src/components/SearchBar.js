8d19c778854da942f9ef22fabf3c49385d76f04a
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-20 16:22:37 -0500

    Add onFormSubmit skeleton

--- pics-app/src/components/SearchBar.js
+++ pics-app/src/components/SearchBar.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
     state = { term: ''};
+    onFormSubmit(event) { 
+        event.preventDefault();
+    }
     render() { 
         return (
           <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
               <div className="field">
                 <label>Image Search</label>
                 <input
