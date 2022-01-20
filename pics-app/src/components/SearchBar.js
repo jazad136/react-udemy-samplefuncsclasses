2cce91c688bdf3e54237da6cffc175c07e4b3972
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-01-20 16:15:10 -0500

    Create a *controlled* input field

--- pics-app/src/components/SearchBar.js
+++ pics-app/src/components/SearchBar.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-    onInputChange(event) { 
-        console.log(event.target.value)
-    }
+    state = { term: ''};
     render() { 
         return (
           <div className="ui segment">
             <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange}/>
-                </div>
+              <div className="field">
+                <label>Image Search</label>
+                <input
+                  type="text"
+                  value={this.state.term}
+                  onChange={(e) => this.setState({ term: e.target.value })}
+                />
+              </div>
             </form>
           </div>
         );
