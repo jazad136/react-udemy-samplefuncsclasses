99e1c5486add304beeefcd61bb91ff15c5f28adf
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-08 11:20:33 -0500

    Make requests to Unsplash after signing up

--- pics-app/src/components/App.js
+++ pics-app/src/components/App.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 class App extends Component { 
     onSearchSubmit(term) { 
-        console.log(term);
+        // Authorization: Client-ID YOUR_ACCESS_KEY
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: {query: term},
+            headers: { 
+                Authorization: 'Client-ID Lj_zHL_K7CFrtEIclKlCDqskD5tNdKbG2W-43LR0YCg'
+            }
+        });
     }
     
     render() { 
