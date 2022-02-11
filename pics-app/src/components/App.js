1492c640c7f03adf851ce770d7ac1df7cd59a182
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-11 14:15:36 -0500

    Refactor axios call

--- pics-app/src/components/App.js
+++ pics-app/src/components/App.js
@@ -1,24 +1,24 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
-class App extends Component { 
-    async onSearchSubmit(term) { 
-        // Authorization: Client-ID YOUR_ACCESS_KEY
-        const response = await axios.get('https://api.unsplash.com/search/photos',{
-            params: {query: term},
-            headers: { 
-                Authorization: 
-                    'Client-ID Lj_zHL_K7CFrtEIclKlCDqskD5tNdKbG2W-43LR0YCg'
-            }
-        });
-        console.log(response.data.results)
+class App extends Component {
+    state = { images: [] };
 
+     onSearchSubmit = async (term) => {
+      // Authorization: Client-ID YOUR_ACCESS_KEY
+      const response = await unsplash.get("/search/photos", {
+        params: { query: term },
+      });
+      // console.log(response.data.results)
+      console.log(this);
+      this.setState({ images: response.data.results });
     }
     
     render() { 
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: { this.state.images.length } images.
             </div>
         );
     }
