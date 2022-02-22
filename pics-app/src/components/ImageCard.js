527525d3e0cf3c2f2619330a0e2f76c0bbc27c2d
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-22 11:28:55 -0500

    Image Card uses Refs

--- pics-app/src/components/ImageCard.js
+++ pics-app/src/components/ImageCard.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) { 
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() { 
+    console.log(this.imageRef);
+    console.log(this.imageRef.current.clientHeight);
+  }
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img 
-          alt={description}
-          src={urls.regular} 
-        />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
