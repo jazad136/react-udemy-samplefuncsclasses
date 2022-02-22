e32db0cca9adb72281c5e5c88c0785a7ac60aaf4
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-22 12:03:51 -0500

    Calculate the number of spans we need per image - 10px units

--- pics-app/src/components/ImageCard.js
+++ pics-app/src/components/ImageCard.js
@@ -3,7 +3,7 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) { 
     super(props);
-
+    this.state = { spans : 0 }; 
     this.imageRef = React.createRef();
   }
 
@@ -16,12 +16,16 @@ class ImageCard extends React.Component {
 
   setSpans = () => { 
     // reach into the dom and figure out height
-    console.log(this.imageRef.current.clientHeight)
+    // console.log(this.imageRef.current.clientHeight)
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10); // + 1
+    this.setState({ spans });
+
   }
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
