bc4a51b3a81750683024b7eb262fdc56dd943d9f
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-22 10:57:44 -0500

    Refactor plain img into ImageCard.js

--- pics-app/src/components/ImageList.js
+++ pics-app/src/components/ImageList.js
@@ -1,9 +1,11 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    const images = props.images.map( ({description, id, urls}) => { 
-        return <img alt={description} key={id} src={urls.regular}/>
+    const images = props.images.map( (image) => { 
+        // return <img alt={description} key={id} src={urls.regular}/>
+        return <ImageCard key={image.id} image={image} />
     })
 
     return <div className='image-list'>{images}</div>
