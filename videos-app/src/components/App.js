07a86591250537ef58d14df486f86faf0c19e0ea
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-22 21:34:30 -0500

    Add App and SearchBar

---
+++ videos-app/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import SearchBar from './SearchBar'
+export class App extends Component {
+  render() {
+    return (
+      <div><SearchBar/></div>
+    )
+  }
+}
+
+export default App
---
+++ videos-app/src/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class SearchBar extends React.Component { 
+    render() { 
+        return <div>SearchBar</div>
+    }
+}
+export default SearchBar;
