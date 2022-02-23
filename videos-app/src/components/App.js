36acf94be23cc6368fdaaddadc048963bb909a2b
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-02-22 21:41:57 -0500

    Add Video Search Text Input

--- videos-app/src/components/App.js
+++ videos-app/src/components/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-import SearchBar from './SearchBar'
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
 export class App extends Component {
   render() {
     return (
-      <div><SearchBar/></div>
-    )
+      <div className="ui container">
+        <SearchBar />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
