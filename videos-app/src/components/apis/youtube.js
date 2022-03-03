82c296aefce75f21fff90909a6e45cc9a7671b86
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-03 14:28:41 -0500

    Add axios call to youtube.js

--- videos-app/src/components/apis/youtube.js
+++ videos-app/src/components/apis/youtube.js
@@ -1,1 +1,11 @@
+import axios from 'axios'
 const KEY = 'AIzaSyC6_nGICnzcaiUfNkWUAEc3TVQz2ILDBmY'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
