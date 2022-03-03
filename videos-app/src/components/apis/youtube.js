92b379b109e398b1dcf8090c574c627bd02ec631
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-03 14:29:54 -0500

    Add type property to youtube.js params

--- videos-app/src/components/apis/youtube.js
+++ videos-app/src/components/apis/youtube.js
@@ -5,6 +5,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
+        type: 'video',
         maxResults: 5,
         key: KEY
     }
