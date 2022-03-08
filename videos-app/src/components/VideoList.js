7556c4976db809f408408e929e915b0e30768228
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-08 11:01:49 -0500

    Add Video Item Component

--- videos-app/src/components/VideoList.js
+++ videos-app/src/components/VideoList.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({videos}) => {
     
-  return (
-    <div>{videos.length}</div>
-  )
+  const renderedList = videos.map((video) => { 
+    return <VideoItem />;
+  });
+  return renderedList;
+  
 }
 
 export default VideoList
