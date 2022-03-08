287bd5bdfc54555798f7a1f5e7b1c907253fd33a
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-08 11:08:59 -0500

    Videos from YouTube show in App

--- videos-app/src/components/VideoList.js
+++ videos-app/src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({videos}) => {
     
   const renderedList = videos.map((video) => { 
-    return <VideoItem />;
+    return <VideoItem video={video} />;
   });
   return renderedList;
   
