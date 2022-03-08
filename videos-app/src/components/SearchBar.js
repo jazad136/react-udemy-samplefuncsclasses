94cc950bfe69f6d0a9d11572eb4f04a2e4e69366
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-08 10:40:02 -0500

    Successfully access YouTube API for video data

--- videos-app/src/components/SearchBar.js
+++ videos-app/src/components/SearchBar.js
@@ -6,11 +6,10 @@ export class SearchBar extends React.Component {
   onInputChange = event => { 
     this.setState({term : event.target.value});
   }
+
   onFormSubmit = event => { 
     event.preventDefault();
-
-    // TODO: Make sure we call
-    // callback from parent component
+    this.props.onFormSubmit(this.state.term)
   }
 
   render() {
