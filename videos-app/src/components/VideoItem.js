bf8578eaa844752972b1b8760b5cf6d8eb81884c
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-08 11:56:56 -0500

    Add CSS Formatting to Video Items

--- videos-app/src/components/VideoItem.js
+++ videos-app/src/components/VideoItem.js
@@ -1,10 +1,13 @@
+import './VideoItem.css'
 import React from 'react';
 
 const VideoItem = ({video}) => { 
     return (
-    <div className="item">
-        <img src={video.snippet.thumbnails.medium.url} />
-        {video.snippet.title}
+    <div className="video-item item">
+        <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+        <div className="content">
+            <div className="header">{video.snippet.title}</div>
+        </div>
     </div>);
 }
 export default VideoItem; 
