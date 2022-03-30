8c9edab7a29ee0958ff21c5f166c2e580bba23f2
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-29 21:21:25 -0400

    Log whenever a semantic-ui title is clicked

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Accordion = ({ items }) => {
-  // return <h1>{items.length}</h1>;
+const Accordion = ( { items } ) => {
+  
+  
+  const onTitleClick = (index) => { 
+    console.log('Title clicked', index)
+  }
+
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
         <div 
             className="title active"
-            onClick={() => console.log('Title clicked', index)}
+            onClick={() => onTitleClick(index)}
         >
           <i className="dropdown icon"></i>
           {item.title}
