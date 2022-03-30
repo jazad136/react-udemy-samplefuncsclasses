12cb5c961ff398a78a6c3d5874984b80edfdab23
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-03-29 21:04:10 -0400

    Initialize basic app with only index/App/Accordion.js

---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Accordion = ({items}) => { 
+    // return <h1>{items.length}</h1>;
+    const renderedItems = items.map(item => {
+        return <React.Fragment key={item.title}>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>{item.content}</p>
+            </div>
+        </React.Fragment>
+    });
+    return <div className="ui styled accordion">
+        {renderedItems}
+    </div>
+}
+
+export default Accordion;
