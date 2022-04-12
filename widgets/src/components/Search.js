516902c46a942a17fd4fe865f20e720d21cd6872
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-04-12 11:53:51 -0400

    Making Wikipedia Request

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const search = async () => { 
+        await axios.get('https://en.wikipedia.org/w/api.php',{
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term
+            }
+        })
+    }
+    search()
+  }, [term]);
 
   return (
     <div className="search">
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input 
-          value={term}
-          onChange={e => setTerm(e.target.value)}
-          className="input"
-          >
-            
-          </input>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          ></input>
         </div>
       </div>
     </div>
